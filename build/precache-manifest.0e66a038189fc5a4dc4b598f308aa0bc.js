63c49e4fca42471deb2192db1d70e8ff268a8dcb
Indanarishi <indana6969@gmail.com>
2020-09-08 14:58:03 +0700

    production

---
+++ build/precache-manifest.0e66a038189fc5a4dc4b598f308aa0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6defbc3d538690c9c7e31fe7a96547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191aa5b821e63d4cdd49",
+    "url": "/static/css/main.a889d10a.chunk.css"
+  },
+  {
+    "revision": "d9d9f03e23ef8f2c1757",
+    "url": "/static/js/2.4615cc22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4615cc22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191aa5b821e63d4cdd49",
+    "url": "/static/js/main.07f4c447.chunk.js"
+  },
+  {
+    "revision": "2cfa20f0f9fb3e8a10bf",
+    "url": "/static/js/runtime-main.22ae3ae3.js"
+  }
+]);
